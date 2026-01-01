ab85792559f41d5cfc7bdf51ee62f8fc227e78d3
Tsuyoshi ISHIDA <ish-ts@wni.com>
2026-01-01 21:41:55 +0900

    feat: ✨ Update README and configuration files for improved setup and usage instructions

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # dotfiles
+
+## Prerequisites
+
+- System git installed
+
+## Initial Setup
+
+Copy the content of [setup.sh](setup.sh) and run it in your terminal.
+
+> [!NOTE]
+> Git clone will be done via `setup.sh`, so no need to clone the repository manually.
+> You can make `setup.sh` executable in your system of course, but it's simpler to just copy and paste it in your terminal.
+
+## Later Updates
+
+After relaunch zsh,
+
+```shell
+jg darwin   # apply nix-darwin configuration
+jg gc       # apply garbage collection to nix store
+```
--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
@@ -11,5 +9,11 @@ export SHELL_SESSIONS_DISABLE=1
 if [[ ! -d "$HOME/Projects" ]]; then
     mkdir -p "$HOME/Projects"
 fi
-
 sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply --source "$HOME/Projects/dotfiles" https://github.com/Hoshock/dotfiles.git
+rm -rf .local/bin/chezmoi
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
+sudo nix run --extra-experimental-features "nix-command flakes" nix-darwin#darwin-rebuild -- switch --flake "$XDG_CONFIG_HOME/nix#$(scutil --get LocalHostName)"
