df3ab0a1b1593771986c10e0f9541fda7a07c6e1
Tsuyoshi ISHIDA <ish-ts@wni.com>
2026-01-01 19:10:32 +0900

    feat: ✨ Add configuration files for various tools and environments

---
+++ dot_config/nix/home-manager/default.nix
@@ -0,0 +1,42 @@
+{ pkgs, username, ... }:
+{
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users."${username}" = {
+    home.stateVersion = "25.11";
+
+    home.packages = with pkgs; [
+      awscli2
+      bat
+      chezmoi
+      codex
+      colima
+      deno
+      eza
+      gh
+      git
+      htop
+      jnv
+      jq
+      just
+      mas
+      nano
+      nixd
+      nixfmt-rfc-style
+      nodePackages.aws-cdk
+      python313Packages.cfn-lint
+      python313Packages.httpie
+      ripgrep
+      sheldon
+      shfmt
+      ssm-session-manager-plugin
+      starship
+      tree
+      uv
+      wget
+      xdg-ninja
+      yq-go
+      zenn-cli
+    ];
+  };
+}
